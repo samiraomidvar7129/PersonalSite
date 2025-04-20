f20f49afd96758e56baf39554a1d035ec5c23a82
samiraomidvar7129 <93695009+samiraomidvar7129@users.noreply.github.com>
2025-04-20 21:43:34 +0400

    import cookieStore

--- my-site/src/i18n/request.js
+++ my-site/src/i18n/request.js
@@ -2,8 +2,9 @@ import {getRequestConfig} from 'next-intl/server';
 import {cookies} from 'next/headers';
 
 export default getRequestConfig(async () => {
-  const cookieStore = cookies();
-  const locale = cookieStore.get('NEXT_LOCALE')?.value || 'en';
+  const cookieStore = await cookies();
+const locale = cookieStore.get('NEXT_LOCALE')?.value || 'en';
+
 
   return {
     locale,
