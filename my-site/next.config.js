7a75e5eb3d8a038ae2308bf94d862f6048524ea3
samiraomidvar7129 <93695009+samiraomidvar7129@users.noreply.github.com>
2025-04-20 15:34:06 +0400

    add output export

--- my-site/next.config.js
+++ my-site/next.config.js
@@ -3,7 +3,7 @@ const withNextIntl = createNextIntlPlugin();
  
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: 'export',
+  output: 'export',
     trailingSlash: true,
       images: {
         unoptimized: true,
